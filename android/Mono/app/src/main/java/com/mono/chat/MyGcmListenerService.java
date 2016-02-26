b9928f9c55ae354d7681214d5c628c8c5384c243
Xuejing Dong <fzudxj@gmail.com>
2016-02-25 21:49:21 -0800

    Redo the confif of gradle files and AndroidManifest to connect GCM

---
+++ android/Mono/app/src/main/java/com/mono/chat/MyGcmListenerService.java
@@ -0,0 +1,9 @@
+package com.mono.chat;
+
+import com.google.android.gms.gcm.GcmListenerService;
+
+/**
+ * Created by xuejing on 2/25/16.
+ */
+public class MyGcmListenerService extends GcmListenerService {
+}
---
+++ android/Mono/app/src/main/java/com/mono/chat/MyInstanceIDListenerService.java
@@ -0,0 +1,9 @@
+package com.mono.chat;
+
+import com.google.android.gms.iid.InstanceIDListenerService;
+
+/**
+ * Created by xuejing on 2/25/16.
+ */
+public class MyInstanceIDListenerService extends InstanceIDListenerService {
+}
