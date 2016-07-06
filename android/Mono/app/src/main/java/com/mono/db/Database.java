db9f53ea0c663246b4dbd2c2309ab305b4fd632e
Gary Ng <garyng02@gmail.com>
2016-07-05 22:12:31 -0700

    Added an additional database method to be able to return the complete query for debugging purposes

--- android/Mono/app/src/main/java/com/mono/db/Database.java
+++ android/Mono/app/src/main/java/com/mono/db/Database.java
@@ -7,6 +7,12 @@ import android.database.sqlite.SQLiteDatabase;
 
 import com.mono.util.Common;
 
+/**
+ * This class is used to encapsulate and extend the SQLite database functions by providing
+ * a range of additional query methods to simplify the effort of writing queries.
+ *
+ * @author Gary Ng
+ */
 public class Database {
 
     private SQLiteDatabase database;
@@ -32,37 +38,59 @@ public class Database {
     }
 
     public Cursor select(String table, String[] projection) {
-        return select(table, projection, null, null, null, null, null, null);
+        return select(table, projection, null, null, null, null, null, null, null);
     }
 
     public Cursor select(String table, String[] projection, String selection,
             String[] selectionArgs) {
-        return select(table, projection, selection, selectionArgs, null, null, null, null);
+        return select(table, projection, selection, selectionArgs, null, null, null, null, null);
     }
 
     public Cursor select(String table, String[] projection, String selection,
             String[] selectionArgs, String sortOrder) {
-        return select(table, projection, selection, selectionArgs, null, sortOrder, null, null);
+        return select(table, projection, selection, selectionArgs, null, sortOrder, null, null,
+            null);
     }
 
     public Cursor select(String table, String[] projection, String sortOrder) {
-        return select(table, projection, null, null, null, sortOrder, null, null);
+        return select(table, projection, null, null, null, sortOrder, null, null, null);
     }
 
     public Cursor select(String table, String[] projection, String selection,
             String[] selectionArgs, String groupBy, String sortOrder, Integer offset,
             Integer limit) {
-        return rawQuery(
-            "SELECT " +
-            Common.implode(", ", projection) +
-            " FROM " + table +
-            (!Common.isEmpty(selection) ? " WHERE " + selection : "") +
-            (!Common.isEmpty(groupBy) ? " GROUP BY " + groupBy : "") +
-            (!Common.isEmpty(sortOrder) ? " ORDER BY " + sortOrder : "") +
-            (limit != null ? " LIMIT " + limit : "") +
-            (offset != null ? " OFFSET " + offset : ""),
-            selectionArgs
+        return select(table, projection, selection, selectionArgs, groupBy, sortOrder, offset,
+            limit, null);
+    }
+
+    public Cursor select(String table, String[] projection, String selection,
+            String[] selectionArgs, String groupBy, String sortOrder, Integer offset,
+            Integer limit, String[] result) {
+        // Construct Query
+        String query = String.format(
+            "SELECT %s FROM %s%s%s%s%s%s",
+            Common.implode(", ", projection),
+            table,
+            !Common.isEmpty(selection) ? " WHERE " + selection : "",
+            !Common.isEmpty(groupBy) ? " GROUP BY " + groupBy : "",
+            !Common.isEmpty(sortOrder) ? " ORDER BY " + sortOrder : "",
+            limit != null ? " LIMIT " + limit : "",
+            offset != null ? " OFFSET " + offset : ""
         );
+        // Return Complete Query
+        if (result != null && result.length > 0) {
+            String str = query;
+
+            if (selectionArgs != null) {
+                for (String arg : selectionArgs) {
+                    str = str.replaceFirst("\\?", arg);
+                }
+            }
+
+            result[0] = str;
+        }
+
+        return rawQuery(query, selectionArgs);
     }
 
     public long insert(String table, ContentValues values) throws SQLException {
