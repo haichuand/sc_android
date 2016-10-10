180da30aca6299a4a38050ed66291152a470ed1b
Gary Ng <garyng02@gmail.com>
2016-10-10 09:47:43 -0700

    Fixed issue where retrieving current location would not always work

--- android/Mono/app/src/main/java/com/mono/util/LocationHelper.java
+++ android/Mono/app/src/main/java/com/mono/util/LocationHelper.java
@@ -2,7 +2,6 @@ package com.mono.util;
 
 import android.content.Context;
 import android.location.Address;
-import android.location.Criteria;
 import android.location.Geocoder;
 import android.location.LocationManager;
 import android.os.AsyncTask;
@@ -15,18 +14,38 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
+/**
+ * This class is used to provide basic location needs such as retrieving the last known location
+ * from the device.
+ *
+ * @author Gary Ng
+ */
 public class LocationHelper {
 
     private static final int MAX_RESULTS = 1;
 
     private LocationHelper() {}
 
+    /**
+     * Check if GPS is enabled.
+     *
+     * @param context Context of application.
+     * @return whether GPS is enabled.
+     */
     public static boolean isGPSEnabled(Context context) {
         LocationManager manager =
             (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
         return manager.isProviderEnabled(LocationManager.GPS_PROVIDER);
     }
 
+    /**
+     * Retrieve bounds with the given coordinates and radius.
+     *
+     * @param latitude Latitude at the center.
+     * @param longitude Longitude at the center.
+     * @param radius Radius in degrees.
+     * @return bound array.
+     */
     public static double[] getLatLngBounds(double latitude, double longitude, float radius) {
         double minLat = latitude - radius;
         if (minLat < -90) minLat = minLat % 90 + 90;
@@ -46,22 +65,35 @@ public class LocationHelper {
         };
     }
 
+    /**
+     * Retrieve last known latitude and longitude cached on the device.
+     *
+     * @param context Context of application.
+     * @return latitude and longitude.
+     */
     public static LatLng getLastKnownLatLng(Context context) {
         LatLng latLng = null;
 
         try {
             LocationManager manager =
                 (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-
-            Criteria criteria = new Criteria();
-            String provider = manager.getBestProvider(criteria, false);
-
-            if (provider != null) {
-                android.location.Location location = manager.getLastKnownLocation(provider);
-
-                if (location != null) {
-                    latLng = new LatLng(location.getLatitude(), location.getLongitude());
+            // Retrieve Location Providers
+            List<String> providers = manager.getProviders(true);
+            android.location.Location location = null;
+            // Determine Best Location
+            for (String provider : providers) {
+                android.location.Location tempLocation = manager.getLastKnownLocation(provider);
+                if (tempLocation == null) {
+                    continue;
                 }
+
+                if (location == null || tempLocation.getAccuracy() < location.getAccuracy()) {
+                    location = tempLocation;
+                }
+            }
+            // Extract Latitude and Longitude
+            if (location != null) {
+                latLng = new LatLng(location.getLatitude(), location.getLongitude());
             }
         } catch (SecurityException e) {
             e.printStackTrace();
@@ -70,6 +102,12 @@ public class LocationHelper {
         return latLng;
     }
 
+    /**
+     * Retrieve last known latitude and longitude cached on the device in the background.
+     *
+     * @param context Context of application.
+     * @param callback Callback for latitude and longitude.
+     */
     public static void getLastKnownLatLng(Context context, LocationCallback callback) {
         new AsyncTask<Object, Void, LatLng>() {
             private LocationCallback callback;
@@ -95,6 +133,15 @@ public class LocationHelper {
         }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, context, callback);
     }
 
+    /**
+     * Retrieve location description with the given latitude and longitude.
+     *
+     * @param context Context of application.
+     * @param latitude Latitude of location.
+     * @param longitude Longitude of location.
+     * @return instance of location.
+     * @throws IOException
+     */
     public static Location getLocationFromLatLng(Context context, double latitude,
             double longitude) throws IOException {
         Location location = null;
@@ -117,6 +164,15 @@ public class LocationHelper {
         return location;
     }
 
+    /**
+     * Retrieve location description with the given latitude and longitude by first checking
+     * for network activity.
+     *
+     * @param context Context of application.
+     * @param latitude Latitude of location.
+     * @param longitude Longitude of location.
+     * @param callback Callback for latitude and longitude.
+     */
     public static void getLocationFromLatLng(final Context context, final double latitude,
             final double longitude, final LocationCallback callback) {
         NetworkHelper.execute(context, new NetworkTask() {
@@ -134,6 +190,12 @@ public class LocationHelper {
         });
     }
 
+    /**
+     * Retrieve last known location cached on the device.
+     *
+     * @param context Context of application.
+     * @param callback Callback for location.
+     */
     public static void getLastKnownLocation(final Context context,
             final LocationCallback callback) {
         NetworkHelper.execute(context, new NetworkTask() {
